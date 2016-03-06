c68e8bac495f7b1bcaf2c7244df15239b4f740f5
Roman Fedorenkov <suffering123494@yandex.ru>
2016-03-06 17:10:41 +0300

    feat(test): added test foe deleteComponent, cancelDeletionComponent actions

--- app/tests/reducers/form_spec.js
+++ app/tests/reducers/form_spec.js
@@ -1,7 +1,7 @@
 import { List, Map } from 'immutable';
 import { expect } from 'chai';
 import { form } from '../../reducers/form';
-import { getDefaultData, addComponent } from '../../actions/form';
+import { getDefaultData, addComponent, deleteComponent, cancelDeletionComponent, updateComponents } from '../../actions/form';
 
 describe('form reducer', () => {
   it('handle INIT', () => {
@@ -17,6 +17,7 @@ describe('form reducer', () => {
       })
     );
   });
+
 
   it('handle ADD_COMPONENT', () => {
     const initialState = form({}, getDefaultData());
@@ -34,5 +35,92 @@ describe('form reducer', () => {
         })),
       }
     ));
+  });
+
+
+  it('handle DELETE_COMPONENT', () => {
+    const initialState = Map({
+      name: 'form',
+      method: 'get',
+      title: 'Form',
+      components: List.of(
+        Map({
+          id: 0,
+          title: 'Default title',
+          formControl: Map(),
+        }),
+        Map({
+          id: 1,
+          title: 'Default title',
+          formControl: Map(),
+        })
+      ),
+    });
+
+    const nextState = form(initialState, deleteComponent({ id: 0 }));
+
+    expect(nextState).to.equal(Map({
+      name: 'form',
+      method: 'get',
+      title: 'Form',
+      components: List.of(
+        Map({
+          id: 0,
+          title: 'Default title',
+          formControl: Map(),
+          isDeleted: true,
+        }),
+        Map({
+          id: 1,
+          title: 'Default title',
+          formControl: Map(),
+        })
+      ),
+    }));
+  });
+
+
+  it('handle CANCEL_DELETION_COMPONENT', () => {
+    const initialState = Map({
+      name: 'form',
+      method: 'get',
+      title: 'Form',
+      components: List.of(
+        Map({
+          id: 0,
+          title: 'Default title',
+          formControl: Map(),
+          isDeleted: true,
+        }),
+        Map({
+          id: 1,
+          title: 'Default title',
+          formControl: Map(),
+          isDeleted: false,
+        })
+      ),
+    });
+
+    const nextState = form(initialState, cancelDeletionComponent({ id: 0 }));
+
+    expect(nextState).to.equal(Map({
+      name: 'form',
+      method: 'get',
+      title: 'Form',
+      components: List.of(
+        Map({
+          id: 0,
+          title: 'Default title',
+          formControl: Map(),
+          isDeleted: false,
+        }),
+        Map({
+          id: 1,
+          title: 'Default title',
+          formControl: Map(),
+          isDeleted: false,
+        })
+      ),
+    }));
   });
 });
