5f6dd433de70bd7bd3504859fb643e53c73cf7c6
Roman Fedorenkov <suffering123494@yandex.ru>
2016-03-11 00:09:08 +0300

    feat(button): added button component

---
+++ app/components/Button/index.jsx
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react';
+import cx from 'classnames';
+
+import './styles.scss';
+
+
+export default class Button extends React.Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    inProgress: PropTypes.bool,
+  }
+
+  render() {
+    const classes = cx('button', this.props.className, {
+      'button_type_in-progress': this.props.inProgress,
+    });
+    return (
+      <button {...this.props} className={classes}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
