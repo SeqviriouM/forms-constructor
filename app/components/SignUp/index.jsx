90607ffeedd745e6fd84fd5b185b8191b54a888e
Roman Fedorenkov <suffering123494@yandex.ru>
2016-03-11 00:20:01 +0300

    feat(signup): added sign up route and basic component

---
+++ app/components/SignUp/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import InfoMessage from 'components/InfoMessage';
+import Input from 'components/Input';
+import Button from 'components/Button';
+import SVGButton from 'components/SVGButton';
+import DocumentTitle from 'react-document-title';
+import './styles.scss';
+
+
+export default class SignUp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.timers = [];
+    this.state = {
+      info: {
+        type: 'info',
+        text: 'Enter your email and password',
+      },
+      shakeInfo: false,
+      inProgress: false,
+    };
+  }
+
+
+  componentWillUnmount = () => {
+    for (const timer of this.timers) {
+      clearTimeout(timer);
+    }
+  }
+
+
+  render() {
+    return (
+      <DocumentTitle title='Sign Up page'>
+        <form
+          className='login'
+        >
+          <InfoMessage
+            className='login__info-message'
+            type={this.state.info.type}
+            shake={this.state.shakeInfo}
+          >{this.state.info.text}</InfoMessage>
+          <Input
+            className='login__input'
+            value={this.state.email}
+            name='email'
+            placeholder='Email'
+          />
+          <Button
+            className='login__submit-button'
+            type='submit'
+            inProgress={this.state.inProgress}
+          >{this.state.inProgress ? 'Sugning ups' : 'Sign Up'}</Button>
+          <SVGButton className='login__submit-button'>
+            Sign Up
+          </SVGButton>
+        </form>
+      </DocumentTitle>
+    );
+  }
+}
--- app/routes/routes.jsx
+++ app/routes/routes.jsx
@@ -3,6 +3,7 @@ import { Router, Route } from 'react-router';
 import App from 'App';
 import LoginPage from 'components/LoginPage';
 import Login from 'components/Login';
+import SignUp from 'components/SignUp';
 
 
 export default (
@@ -10,6 +11,7 @@ export default (
   <Route path='/' component={App} />
   <Route path='/' component={LoginPage}>
     <Route path='login' component={Login} />
+    <Route path='signup' component={SignUp} />
   </Route>
   </Router>
 );
