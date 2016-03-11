26ebaa9e74f8c7eaea8be1043e762a37fe79a022
Roman Fedorenkov <suffering123494@yandex.ru>
2016-03-11 12:19:19 +0300

    fix(tabs): fixed getting currentTabId

--- app/components/Tabs/index.jsx
+++ app/components/Tabs/index.jsx
@@ -1,7 +1,12 @@
 import React, { PropTypes, cloneElement } from 'react';
+import { connect } from 'react-redux';
+import { pushState } from 'redux-router';
 import cx from 'classnames';
 import './styles.scss';
 
+@connect(state => ({
+  location: state.router.location.pathname,
+}), { pushState })
 export default class Tabs extends React.Component {
 
   static propTypes = {
@@ -9,12 +14,16 @@ export default class Tabs extends React.Component {
     changeTab: PropTypes.func,
     className: PropTypes.string,
     children: PropTypes.node.isRequired,
+    location: PropTypes.string.isRequired,
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      currentTabId: this.props.currentTabId || 1,
+      currentTabId: this.props.currentTabId || (path => {
+        const tabIndex = this.props.children.findIndex(tabElement => tabElement.props.link === path);
+        return tabIndex > -1 ? (tabIndex + 1) : 1;
+      })(this.props.location),
     };
   }
 
@@ -36,6 +45,7 @@ export default class Tabs extends React.Component {
     const { className, currentTabId, changeTab } = this.props;
     const children = [].concat(this.props.children);
     const tabWidth = `${100 / children.length}%`;
+    const currentTabIndex = (currentTabId || this.state.currentTabId) - 1;
 
     const getChildren = () => children.map((tabElement, i) => cloneElement(tabElement, {
       isCurrent: (currentTabId || this.state.currentTabId) === tabElement.props.id,
@@ -49,7 +59,7 @@ export default class Tabs extends React.Component {
       <div className={cx('tabs', className)}>
         <div className='tabs__hr-wrapper'>
           <hr className='tabs__hr' style={{
-            left: `${parseInt(tabWidth, 10) * (this.state.currentTabId - 1)}%`,
+            left: `${parseInt(tabWidth, 10) * (currentTabIndex)}%`,
             width: `${tabWidth}`,
           }}
           />
