0425148b0ce074cd94b3509eeb3e898e0b504cb7
Roman Fedorenkov <suffering123494@yandex.ru>
2016-05-04 21:12:09 +0300

    feat(signup): added ability to sign up

--- app/components/SignUp/index.jsx
+++ app/components/SignUp/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import cx from 'classnames';
+import store from 'store';
 import InfoMessage from 'components/InfoMessage';
 import Input from 'components/Input';
 import Button from 'components/Button';
@@ -17,6 +19,12 @@ export default class SignUp extends React.Component {
         type: 'info',
         text: 'Enter your email and password',
       },
+      email: '',
+      password: '',
+      repeatedPassword: '',
+      showEmailError: false,
+      showPasswordError: false,
+      showRepeatedPasswordError: false,
       shakeInfo: false,
       inProgress: false,
     };
@@ -30,29 +38,171 @@ export default class SignUp extends React.Component {
   }
 
 
+  signUp = (e) => {
+    e.preventDefault();
+
+    const email = this.state.email;
+    const password = this.state.password;
+    const repeatedPassword = this.state.repeatedPassword;
+
+    if (!email) {
+      return this.setState({
+        showEmailError: true,
+        info: {
+          type: 'error',
+          text: 'Email is required',
+        },
+      });
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      return this.setState({
+        showEmailError: true,
+        info: {
+          type: 'error',
+          text: 'Valid email is required',
+        },
+      });
+    }
+
+    if (!password) {
+      return this.setState({
+        showPasswordError: true,
+        showSecondPasswordError: true,
+        info: {
+          type: 'error',
+          text: 'Password is required',
+        },
+      });
+    }
+
+    if (password !== repeatedPassword) {
+      return this.setState({
+        showSecondPasswordError: true,
+        info: {
+          type: 'error',
+          text: 'The passwords don\'t match. Please check and try again.',
+        },
+      });
+    }
+
+    const signUpData = {
+      email,
+      password,
+      repeatedPassword,
+    };
+
+    this.setState({
+      inProgress: true,
+    });
+
+    fetch('/signup', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(signUpData),
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then(() => {
+          store.history.pushState(null, '/');
+        });
+      }
+    });
+  }
+
+
+  checkEmailExist = (email) => {
+    this.setState({
+      showEmailError: false,
+      email,
+    });
+  }
+
+
+  emailChange = (e) => {
+    const email = e.target.value;
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      return this.setState({
+        showEmailError: true,
+        info: {
+          type: 'error',
+          text: 'Valid email is required',
+        },
+      });
+    }
+
+    this.checkEmailExist(email);
+  }
+
+
+  passwordChange = (e) => {
+    const password = e.target.value;
+
+    if (password.length) {
+      this.setState({
+        password,
+        showPasswordError: false,
+        showRepeatedPasswordError: false,
+      });
+    }
+  }
+
+
+  repeatedPasswordChange = (e) => {
+    const repeatedPassword = e.target.value;
+
+    if (repeatedPassword.length) {
+      this.setState({
+        repeatedPassword,
+        showPasswordError: false,
+        showRepeatedPasswordError: false,
+      });
+    }
+  }
+
+
   render() {
     return (
       <DocumentTitle title='Sign Up page'>
         <form
-          className='login'
+          className='sign-up'
+          onSubmit={this.signUp}
         >
           <InfoMessage
-            className='login__info-message'
+            className='sign-up__info-message'
             type={this.state.info.type}
             shake={this.state.shakeInfo}
           >{this.state.info.text}</InfoMessage>
           <Input
-            className='login__input'
+            className={cx('sign-up__input', {
+              input_type_error: this.state.showEmailError,
+              input_type_success: !this.state.showEmailError && this.state.email,
+            })}
             value={this.state.email}
-            name='email'
             placeholder='Email'
+            onChange={this.emailChange}
+          />
+          <Input
+            className='sign-up__input'
+            value={this.state.password}
+            placeholder='Password'
+            onChange={this.passwordChange}
+          />
+          <Input
+            className='sign-up__input'
+            value={this.state.repeatedPassword}
+            placeholder='Repeat password'
+            onChange={this.repeatedPasswordChange}
           />
           <Button
-            className='login__submit-button'
+            className='sign-up__submit-button'
             type='submit'
             inProgress={this.state.inProgress}
-          >{this.state.inProgress ? 'Sugning ups' : 'Sign Up'}</Button>
-          <SVGButton className='login__submit-button'>
+          >{this.state.inProgress ? 'Signing up' : 'Sign Up'}</Button>
+          <SVGButton className='sign-up__submit-button'>
             Sign Up
           </SVGButton>
         </form>
