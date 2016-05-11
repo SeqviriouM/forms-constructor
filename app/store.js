f39006066fd52f252f5d3abf4037730e3ed7ad94
Roman Fedorenkov <suffering123494@yandex.ru>
2016-05-11 12:36:55 +0300

    feat(store): added socket middleware

--- app/store.js
+++ app/store.js
@@ -1,11 +1,20 @@
-import { compose, createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { persistState } from 'redux-devtools';
 import { appReducer } from 'reducers/reducer';
 import { reduxReactRouter } from 'redux-router';
 import { createHistory } from 'history';
 import routes from 'routes/routes';
+import socketClient from 'core/socket';
+
+const socketMiddleware = () => next => action => {
+  if (action.send) {
+    socketClient(action.type, action.payload);
+  }
+  return next(action);
+};
 
 const store = compose(
+  applyMiddleware(socketMiddleware),
   reduxReactRouter({
     createHistory,
     routes,
